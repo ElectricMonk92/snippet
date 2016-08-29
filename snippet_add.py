9858d1c09836447fec4b2e707e95f0068aa0fe45
Mark Jackson <truebrit.jackson@gmail.com>
2016-08-30 00:30:04 +0100

    Implemented simple add functionality with tinyDB

--- snippet_add.py
+++ snippet_add.py
@@ -1,3 +1,24 @@
+from tinydb import TinyDB, Query
+import constants
 
 def add(args):
-    print("add")
+    print args
+    name    = args.name
+    tags    = args.tags
+    content = args.content
+
+    print("Name {}".format(name))
+    print("Tags {}".format(tags))
+    print("Content {}".format(content))
+
+    snippet_to_insert = {
+        constants.NAME_FIELD    : name,
+        constants.TAGS_FIELD    : tags,
+        constants.CONTENT_FIELD : content
+    }
+
+    db = TinyDB('db.json')
+    snippets = db.table(constants.SNIPPETS_TABLE)
+    snippets.insert(snippet_to_insert)
+
+    print "Sucessfully inserted snippet: ", name
